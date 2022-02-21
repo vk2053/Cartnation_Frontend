9a23fc05327cfda5db55083207488f78c48d6185
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air.local>
2022-02-21 17:33:11 +0530

    card and carousel adding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import Carouselcomp from './components/Carousel';
+import Cards from './components/Card';
 import App from './App';
 
 ReactDOM.render(
